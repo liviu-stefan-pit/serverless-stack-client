e812fc4970f2c816579310e97523522b615f0b51
liviustefan <liviustefan.petroaia@gmail.com>
2020-03-21 13:54:41 +0200

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "erp-system-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://ao44tf9f62.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_XoSwXn9jG",
+      APP_CLIENT_ID: "hmsd9924q9qd0sdnlo029amsu",
+      IDENTITY_POOL_ID: "us-east-2:de79a15e-2e62-4a4e-9c41-76f66cf10155"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Tracker</h1>
+        <p>A simple time registration app</p>
+      </div>
+    </div>
+  );
+}
