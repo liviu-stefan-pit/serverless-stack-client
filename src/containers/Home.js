95c708d7c5739a71f21bf0e747e6c836e0fdc3f3
liviustefan <liviustefan.petroaia@gmail.com>
2020-03-29 19:30:43 +0300

    Updating landing page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
@@ -54,8 +55,16 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Tracker</h1>
-        <p>A simple time management app</p>
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
